336f5e9ea30b3ba35532c0528715f859405bb74b
Gregori Spielmann <gregorispielmann@gmail.com>
2019-09-18 20:10:00 -0300

    signup / login

---
+++ src/api/user/authService.js
@@ -0,0 +1,93 @@
+const _ = require("lodash");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("./user");
+const env = require("../../.env");
+
+const emailRegex = /\S+@\S+\.\S+/;
+const passwordRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%]).{6,20})/;
+
+// método para enviar as mensagens de erro
+const sendErrorsFromDB = (res, dbErrors) => {
+  const errors = [];
+  _.forIn(dbErrors.errors, error => errors.push(error.message));
+  return res.status(400).json({ errors });
+};
+
+// método para fazer o login
+const login = (req, res, next) => {
+  const email = req.body.email || "";
+  const password = req.body.password || "";
+
+  User.findOne({ email }, (err, user) => {
+    if (err) {
+      return sendErrorsFromDB(res, err);
+    } else if (user && bcrypt.compareSync(password, user.password)) {
+      const token = jwt.sign(user, env.authSecret, {
+        expiresIn: "1 day"
+      });
+      const { name, email } = user;
+      res.json({ name, email, token });
+    } else {
+      return res.status(400).send({ errors: ["Usuário/Senha inválidos"] });
+    }
+  });
+};
+
+// método para validar o token
+const validateToken = (req, res, next) => {
+  const token = req.body.token || "";
+  jwt.verify(token, env.authSecret, function(err, decoded) {
+    return res.status(200).send({ valid: !err });
+  });
+};
+
+// método para criar cadastro no db
+const signup = (req, res, next) => {
+  const name = req.body.name || "";
+  const email = req.body.email || "";
+  const password = req.body.password || "";
+  const confirmPassword = req.body.confirm_password || "";
+
+  if (!email.match(emailRegex)) {
+    return res
+      .status(400)
+      .send({ errors: ["O e-mail informado está inválido"] });
+  }
+  if (!password.match(passwordRegex)) {
+    return res.status(400).send({
+      errors: [
+        "Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial (@#$%) e tamanho entre 6-20 dígitos."
+      ]
+    });
+  }
+
+  // método para verificar a senha do db com base na senha criptografada
+  const salt = bcrypt.genSaltSync();
+  const passwordHash = bcrypt.hashSync(password, salt);
+  if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
+    return res.status(400).send({ errors: ["Senhas não conferem."] });
+  }
+
+  // método para checar usuário no db
+  User.findOne({ email }, (err, user) => {
+    if (err) {
+      return sendErrorsFromDB(res, err);
+    } else if (user) {
+      return res.status(400).send({ errors: ["Usuário já cadastrado."] });
+    } else {
+      const newUser = new User({ name, email, password: passwordHash });
+      newUser.save(err => {
+        if (err) {
+          return sendErrorsFromDB(res, err);
+        } else {
+          login(req, res, next);
+        }
+      });
+    }
+  });
+
+};
+
+
+module.exports = { login, signup, validateToken }
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,16 +1,26 @@
-const express = require('express')
+const express = require("express");
+const auth = require("./auth");
 
-module.exports = function(app){
+module.exports = function(server) {
+  /*
+   * Rotas protegidas por Token JWT
+   */
+  const protectedApi = express.Router();
 
-    // BASE URL
-    const router = express.Router()
-    app.use('/api', router)
-
-
-    //PAYMENT CYCLES ROUTES
-    const BillingCycle = require('../api/billingCycles/billingCycleService')
-
-    BillingCycle.register(router, '/billingCycles')
-
-
-}
+  server.use("/api", protectedApi);
+  protectedApi.use(auth);
+  
+  const BillingCycle = require("../api/billingCycles/billingCycleService");
+  BillingCycle.register(protectedApi, "/billingCycles");
+  
+  /*
+   * Rotas abertas
+   */
+  const openApi = express.Router();
+  server.use("/oapi", openApi);
+  
+  const AuthService = require("../api/user/AuthService");
+  openApi.post("/login", AuthService.login);
+  openApi.post("/signup", AuthService.signup);
+  openApi.post("/validateToken", AuthService.validateToken);
+};
